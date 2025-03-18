c5e52ebf112a4788076b4752a7584d3c92104c8c
HildaAb <ilona.aase@gmail.com>
2025-03-18 19:07:14 +0100

    fixed dark/light mode

--- app.js
+++ app.js
@@ -11,13 +11,16 @@ closeBtn.addEventListener("click", () => {
   sideMenu.style.display = "none";
 });
 
+/*fundera*/
+
 themeToggler.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme-variables");
 
-  themeToggler.querySelector("i:nth-child(1)").classList.toggle(".active");
-  themeToggler.querySelector("i:nth-child(2)").classList.toggle(".active");
+  themeToggler.querySelector("i:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("i:nth-child(2)").classList.toggle("active");
 });
 
+/*fundera*/
 orders.forEach((order) => {
   const tr = document.querySelector("tr");
   const trContent = `
