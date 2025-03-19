23fb689a1dc292f42371eaf36aff8e1b3cd5bb7c
HildaAb <ilona.aase@gmail.com>
2025-03-19 09:34:15 +0100

    fixed displaying js orders

--- src/app.js
+++ src/app.js
@@ -21,22 +21,19 @@ themeToggler.addEventListener("click", () => {
 });
 
 /*fundera*/
+
 orders.forEach((order) => {
-  const tr = document.querySelector("tr");
+  const tr = document.createElement("tr");
   const trContent = `
      <td>${order.productName}</td> 
        <td>${order.productNumber}</td>
         <td>${order.paymentStatus}</td>
-         <td class="${
-           order.shipping === "Declined"
-             ? "danger"
-             : order.shipping === "pending"
-             ? "warnning"
-             : "primary"
-         }">${order.shipping}
-         </td> 
-         <td class="primary">Deta(ils</td>
+         <td>${order.shipping}</td> 
+         <td class="primary">Details</td>
     `;
+
   tr.innerHTML = trContent;
+  console.log(trContent);
+
   document.querySelector("table tbody").appendChild(tr);
 });
---
+++ src/orders.js
@@ -0,0 +1,68 @@
+const orders = [
+  {
+    productName: "Oil paintinng",
+    productNumber: "98765",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Oil brush",
+    productNumber: "98699",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+  {
+    productName: "Oil paintinng",
+    productNumber: "56789",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Oil paintinng",
+    productNumber: "56789",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Oil cleaning",
+    productNumber: "12349",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Oil paintinng",
+    productNumber: "48889",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Oil paintinng",
+    productNumber: "98765",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Oil brush",
+    productNumber: "98699",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+  {
+    productName: "Oil paintinng",
+    productNumber: "56789",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Oil paintinng",
+    productNumber: "98765",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Oil brush",
+    productNumber: "98699",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+];
